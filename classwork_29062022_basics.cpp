026fd4ecb70c0c29f04fc7dc6734a97ec5e4caa7
Somdev Mitra <somdev_55@outlook.com>
2022-06-29 19:07:22 +0530

    First Code of 29.06.2022

--- classwork_29062022_basics.cpp
+++ classwork_29062022_basics.cpp
@@ -1,3 +1,16 @@
 #include<iostream>
 using namespace std;
-int main() 
+int main(){
+   int height;
+   cout << "Enter the height:"<< endl;
+   cin >> height;
+   for (int j=0; j<=height;j++)
+   {
+    for(int i=0; i<=j; i++)
+    {
+        cout<<"*";
+    }
+    cout << "\n";
+   } 
+   return 0;
+}
