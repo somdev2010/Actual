1e6e0b37d5e429ced290b806eb2386d336aad818
Somdev Mitra <somdev_55@outlook.com>
2022-10-08 12:39:48 +0530

    odd_even correct

--- if_else.cpp
+++ if_else.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main(){
     int a;
     cin>>a;
-    if (a%2=0){
+    if (a%2==0){
         cout<<"The number is even";
     }else{
         cout<<"The number is odd";
     }
 return 0;
 }
+//even-odd check
